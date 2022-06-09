5bb326c832bf97282ec557a24c619399bd0b2962
Joseph Feliciano <xyndronix1@gmail.com>
2022-06-09 19:11:00 +0000

    Change ArrayList Item. Add communication Div in HTML. Style communications Div in CSS

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -17,7 +17,7 @@ public class HelloWorldServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     messages.add("My Favorite Game Of All Time Is Sonic Unleashed!");
-    messages.add("My Favorite Movie Of All Time Is John Carpenter's Christine!");
+    messages.add("My Favorite Movie Of All Time Is Ultraviolet!");
     messages.add("My Favorite Food Is Some Delicious Pizza!");
     String messagesAsJSON = JsonViaGson(messages);
     response.setContentType("application/json;");
