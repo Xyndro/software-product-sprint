1d26f918680401128a773bbd535f1ae355b911f8
Joseph Feliciano <xyndronix1@gmail.com>
2022-06-09 18:39:51 +0000

    Add ArrayList of messages and Add JsonViaGson Conversion Method

--- portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/HelloWorldServlet.java
@@ -1,18 +1,31 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.gson.Gson;
 
 /** Handles requests sent to the /hello URL. Try running a server and navigating to /hello! */
 @WebServlet("/hello")
 public class HelloWorldServlet extends HttpServlet {
 
+    private ArrayList<String> messages = new ArrayList<String>();  
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    messages.add("My Favorite Game Of All Time Is Sonic Unleashed!");
+    messages.add("My Favorite Movie Of All Time Is John Carpenter's Christine!");
+    messages.add("My Favorite Food Is Some Delicious Pizza!");
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Joseph!</h1>");
+    response.getWriter().println(messages);
+  }
+
+  public String JsonViaGson(ArrayList<String> messages){
+      Gson gson = new Gson();
+      String toJSON = gson.toJson(messages);
+      return toJSON;
   }
 }
